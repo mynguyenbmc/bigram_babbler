dea6e0db3c5fa72b21097297e40f06f953b4fdd3
mynguyenbmc <mnguyen1@brynmawr.edu>
2018-06-01 01:00:13 -0400

    optimized, done

--- Main.hs
+++ Main.hs
@@ -1,45 +1,35 @@
 module Main where
 import System.IO
 import Data.HashMap.Strict as H
+import Data.List
 import System.Random as R
--- import Data.Random as R
 
 insertBigrams :: [String] -> HashMap String [String]-> HashMap String [String]
-insertBigrams[] h = h
+insertBigrams [] h = h
 insertBigrams (x : []) h = h
-insertBigrams (x : remains@(x' : xs)) h
- | H.member x h == True = insertBigrams remains $ H.insert x (x' : (extract $ H.lookup x h)) h
- | otherwise = insertBigrams remains $ H.insert x [x'] h
- where
-   extract :: Maybe [String] -> [String]
-   extract (Just l) = l
+insertBigrams (x : rest@(x' : xs)) h = insertBigrams rest $ H.insertWith f x [x'] h
+   where f a b = a ++ b
 
 printText :: (RandomGen g) => Int -> g -> String -> HashMap String [String] -> IO ()
-printText 0 _ _ _ = putStr ""
-printText n g k h = do
-  putStr (k ++ " ")
-  g <- R.newStdGen
-  (printText (n-1) g nextk h)
-    where
-      lis = extract $ H.lookup k h
-      extract (Just str) = str
-      nextk = pick lis g
+printText 0 _ _ _ = putStr "\n"
+printText n g k h = case H.lookup k h of
+  Nothing -> error "invalid input"
+  Just v -> do
+             putStr (k ++ " ")
+             printText (n-1) nextG nextK h
+                where (nextK, nextG) = pick v g
 
-pick :: (RandomGen g) => [a] -> g -> a
-pick xs g = xs !! fst (R.randomR (0, length xs - 1) g)
+pick :: (RandomGen g) => [a] -> g -> (a,g)
+pick xs g = (xs !! a, ng)
+   where (a,ng) = R.randomR (0, length xs - 1) g
 
 main = do
   handle <- readFile "alice30.txt"
-  let line = lines handle
-  let wordlist = words handle
-  let bigrams = insertBigrams wordlist newMap
+  let wordlist = concatMap (\x -> words x) $ lines handle
+  let bigrams = insertBigrams wordlist H.empty
   putStrLn "Enter number of words to print out: "
   getAnswer <- getLine
   let num = read getAnswer :: Int
   g <- R.newStdGen
-  let firstword = pick (H.keys bigrams) g
+  let (firstword,_) = pick (H.keys bigrams) g
   printText num g firstword bigrams
-  print num
-  where
-    newMap :: HashMap String [String]
-    newMap = H.empty
